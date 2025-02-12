0a5593971c95b09f248d6d49479d8c812a139986
Mike McGann <mike.mcgann@blackchip.org>
2025-02-11 20:46:53 -0500

    with attract

--- scripts/attract.lua
+++ scripts/attract.lua
@@ -5,8 +5,87 @@ local jd = require("jd")
 local pub = {}
 
 function pub.attract()
+    spin.run(jd.ATTRACT_SLIDE_SHOW)
     spin.wait(spin.for_eq(std.GAME_ACTIVE, true))
     spin.run(jd.MAIN)
+end
+
+function pub.attract_slide_show()
+    local slides = {
+        jd.ATTRACT_GAME_OVER,
+        jd.ATTRACT_DTP_PRESENTS,
+        jd.ATTRACT_JD_REMIX,
+        jd.ATTRACT_PAYMENT,
+        jd.ATTRACT_GC
+    }
+    local n = 5
+    local curr = 1
+
+    local next = function()
+        curr = curr + 1
+        if curr > 5 then
+            curr = 1
+        end
+    end
+
+    while true do
+        spin.run(slides[curr])
+        spin.sleep(4)
+        next()
+    end
+end
+
+function pub.attract_game_over()
+    local gfx = spin.gfx(jd.DMD)
+    gfx.clear()
+    gfx.font(jd.PF_RONDA_SEVEN_BOLD_8)
+    gfx.draw_centered_text("GAME OVER")
+end
+
+function pub.attract_dtp_presents()
+    local gfx = spin.gfx(jd.DMD)
+    gfx.clear()
+    gfx.font(jd.PF_ARMA_FIVE_8)
+    gfx.draw_text_y(3, "DROP TARGET PINBALL")
+    gfx.font(jd.PF_RONDA_SEVEN_BOLD_8)
+    gfx.draw_text_y(14, "PRESENTS")
+end
+
+function pub.attract_jd_remix()
+    local gfx = spin.gfx(jd.DMD)
+    gfx.clear()
+    gfx.font(jd.PF_RONDA_SEVEN_BOLD_8)
+    gfx.draw_text_y(3, "JUDGE DREDD")
+    gfx.draw_text_y(14, "REMIX")
+end
+
+function pub.attract_payment()
+    local gfx = spin.gfx(jd.DMD)
+    gfx.font(jd.PF_RONDA_SEVEN_BOLD_8)
+
+    local draw = function(on)
+        gfx.clear()
+        if on then
+            gfx.draw_text_y(3, "PRESS START")
+        end
+        gfx.draw_text_y(14, "FREE PLAY")
+    end
+
+    for i=1,5 do
+        draw(true)
+        spin.sleep(0.2)
+        draw(false)
+        spin.sleep(0.1)
+    end
+    draw(true)
+end
+
+function pub.attract_gc()
+    local gfx = spin.gfx(jd.DMD)
+    gfx.clear()
+    gfx.font(jd.PF_RONDA_SEVEN_BOLD_8)
+    gfx.draw_text_y(3, "GRAND CHAMPION")
+    gfx.draw_text_y(14, "MCG  500,000,000")
 end
 
 return pub
--- scripts/boot.lua
+++ scripts/boot.lua
@@ -6,12 +6,13 @@ local pub = {}
 function pub.boot()
     local gfx = spin.gfx(jd.DMD)
     gfx.clear()
-    gfx.dot_on()
-    gfx.font(jd.PF_TEMPESTA_FIVE_COMPRESSED_BOLD)
-    gfx.draw_text_y(4, string.upper(spin.runtime.prog_description))
-    gfx.draw_text_y(14, "V" .. string.upper(spin.runtime.prog_version))
-    gfx.draw_text_y(24, string.upper(spin.runtime.prog_date))
-    spin.play_music(jd.BOOT)
+    gfx.font(jd.PF_TEMPESTA_FIVE_COMPRESSED_BOLD_8)
+    gfx.draw_text_y(0, string.upper(spin.runtime.prog_description))
+    gfx.draw_text_y(10, "V" .. string.upper(spin.runtime.prog_version))
+    gfx.draw_text_y(20, string.upper(spin.runtime.prog_date))
+    spin.play_music(jd.BOOT, {loops=1})
+    spin.sleep(8)
+    spin.run(jd.ATTRACT)
 end
 
 return pub
