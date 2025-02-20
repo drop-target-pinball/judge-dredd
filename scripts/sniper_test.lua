64335e460616025dbc325e5b6aa8c1fc5749ac94
Mike McGann <mike.mcgann@blackchip.org>
2025-02-19 23:21:14 -0500

    test

--- scripts/sniper_test.lua
+++ scripts/sniper_test.lua
@@ -1,6 +1,6 @@
 local spin = require('spin')
-local std = require('std')
 local jd = require('jd')
+local test = require('test')
 
 local pub = {}
 
@@ -8,6 +8,14 @@ function pub.test_sniper_failure()
     spin.run(jd.SNIPER)
     spin.sleep(8)
     spin.set(jd.SNIPER_SCORE, 3550700)
+end
+
+function pub.test_sniper_success()
+    spin.run(jd.SNIPER)
+    spin.sleep(4)
+    spin.switch_updated(jd.RIGHT_POPPER)
+    spin.sleep(3)
+    spin.switch_updated(jd.RIGHT_POPPER)
 end
 
 return pub
