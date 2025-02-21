272fd15f3e965203ecd12a33e89f0a8f01d554eb
Mike McGann <mike.mcgann@blackchip.org>
2025-02-20 20:06:52 -0500

    wip

--- scripts/sniper_test.lua
+++ scripts/sniper_test.lua
@@ -8,6 +8,7 @@ function pub.test_sniper_failure()
     spin.run(jd.SNIPER)
     spin.sleep(8)
     spin.set(jd.SNIPER_SCORE, 3550700)
+    test.wait(10, "script end", spin.for_script(jd.SNIPER))
 end
 
 function pub.test_sniper_fall_failure()
@@ -16,6 +17,7 @@ function pub.test_sniper_fall_failure()
     spin.switch_updated(jd.RIGHT_POPPER)
     spin.sleep(7)
     spin.set(jd.SNIPER_FALL_TIMER, 2)
+    test.wait(10, "script end", spin.for_script(jd.SNIPER_FAILURE))
 end
 
 function pub.test_sniper_success()
@@ -24,6 +26,7 @@ function pub.test_sniper_success()
     spin.switch_updated(jd.RIGHT_POPPER)
     spin.sleep(7)
     spin.switch_updated(jd.RIGHT_POPPER)
+    test.wait(10, "script end", spin.for_script(jd.SNIPER_SUCCESS))
 end
 
 return pub
